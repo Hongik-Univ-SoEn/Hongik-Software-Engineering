e183c921857b0b7e8e4beaddf43b713594cff225
JUNStone <j00mst0m3@gmail.com>
2023-05-26 06:56:16 +0900

    revert code to before write comment

--- Hongik-Software-Engineering/source/Boundaries/Hire/AddRecruitUI.cpp
+++ Hongik-Software-Engineering/source/Boundaries/Hire/AddRecruitUI.cpp
@@ -16,29 +16,23 @@
     반환값 : 없음
   */
 void AddRecruitUI::startInterface() {
-    char companyName[MAX_STRING], workField[MAX_STRING], deadline[MAX_STRING], businessNumber [MAX_STRING];
-    char numOfHire[MAX_STRING];
+    char workField[MAX_STRING], numOfHire[MAX_STRING], deadline[MAX_STRING];
 
-
-    CompanyMember* companyMember = (CompanyMember*) Member::getInstance();
-    companyMember->getInfo(); //현재 companyMember의 companyName과 businessNumber 받아온다
-
-    
     fprintf(out_fp, "3.1. 채용 정보 등록\n>");
-    fscanf(in_fp, " %s %s %s", workField, numOfHire, deadline); //파일로부터 업무, 인원 수, 신청 마감일을 읽어봐서 반환
+    fscanf(in_fp, " %s %s %s", workField, numOfHire, deadline); 
+    auto info = ((CompanyMember*)Member::getInstance())->getInfo();
 
-   
-    vector<string> info;
-    info.push_back(companyName);
-    info.push_back(businessNumber);
-    info.push_back(workField);
-    info.push_back(numOfHire);
-    info.push_back(deadline); // vector<string> info로 받은 값들을 리스트에 넣어서 채용 공고로 등록 시킨다.
+    vector<string> v;
+    v.push_back(info[0]);
+    v.push_back(info[1]);
+    v.push_back(workField);
+    v.push_back(numOfHire);
+    v.push_back(deadline);
+    control->addRecruit(v);
 
-    fprintf(out_fp, " %s %d %s", workField, &numOfHire, deadline);
-    fprintf(out_fp, "\n"); // 받은 결과 값을 파일에 출력.
+    fprintf(out_fp, " %s %s %s", workField, numOfHire, deadline);
+    fprintf(out_fp, "\n\n");
 }
-
 /*
    함수 이름   : AddRecruitUI::setControl(AddRecruit* control)
    기능  : 외부에서 AddRecruit class (control class)의 포인터 객체를 전달 받아, AddRecruitUI(boundary class)가 AddRecruit(control class)를 참조하도록 속성값으로 저장.

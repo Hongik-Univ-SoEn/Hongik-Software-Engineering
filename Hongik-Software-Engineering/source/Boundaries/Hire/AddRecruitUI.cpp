df703341ac2680f8762ed9c013118e545871bb2c
chanheap <parc57165@naver.com>
2023-05-26 06:07:41 +0900

    AddRecruit,UI 주석 추가..

--- Hongik-Software-Engineering/source/Boundaries/Hire/AddRecruitUI.cpp
+++ Hongik-Software-Engineering/source/Boundaries/Hire/AddRecruitUI.cpp
@@ -11,29 +11,33 @@
 
  
 void AddRecruitUI::startInterface() {
-    char workField[MAX_STRING], deadline[MAX_STRING];
+    char companyName[MAX_STRING], workField[MAX_STRING], deadline[MAX_STRING], businessNumber [MAX_STRING];
     char numOfHire[MAX_STRING];
 
-    auto info = ((CompanyMember*)Member::getInstance())->getInfo();
+    // 컴페니 멤버로 Member클래스의 getInstane() 호출하여 companyMember, businessNumber를 가져온다.
+    CompanyMember* companyMember = (CompanyMember*) Member::getInstance();
+    companyMember->getInfo();
 
+    //파일 입력을 받아 업무, 인원 수, 신청 마감일에 할당하고
     fprintf(out_fp, "3.1. 채용 정보 등록\n>");
-    fscanf(in_fp, "%s %s %s", workField, numOfHire, deadline);
+    fscanf(in_fp, " %s %s %s", workField, numOfHire, deadline);
 
-    vector<string> v;
-    v.push_back(info[0]);
-    v.push_back(info[1]);
-    v.push_back(workField);
-    v.push_back(numOfHire);
-    v.push_back(deadline);
-    control->addRecruit(v);
+    //앞에서 받은 인자들을 vector<string> info 리스트에 넣어서
+    //회사회원이 올린 채용공고로 등록을 시킨다.
+    vector<string> info;
+    info.push_back(companyName);
+    info.push_back(businessNumber);
+    info.push_back(workField);
+    info.push_back(numOfHire);
+    info.push_back(deadline);
 
-    fprintf(out_fp, " %s %s %s", workField, &numOfHire, deadline);
-    fprintf(out_fp, "\n\n");
+    //txt 출력 파일에 출력한다.
+    fprintf(out_fp, " %s %d %s", workField, &numOfHire, deadline);
+    fprintf(out_fp, "\n");
 }
 
-/**
- * @param workField
- */
+
 void AddRecruitUI::setControl(AddRecruit* control) {
+   
    this->control = control;
 }
--- Hongik-Software-Engineering/source/Controls/Hire/AddRecruit.cpp
+++ Hongik-Software-Engineering/source/Controls/Hire/AddRecruit.cpp
@@ -4,23 +4,22 @@
 
 
 #include "AddRecruit.h"
-#include "../../Entities/RecruitCollection.h"
-#include "../../Entities/CompanyMember.h"
+#include "./../../Entities/RecruitCollection.h"
+#include "./../../Entities/CompanyMember.h"
 
 using namespace std;
 /**
  * AddRecruit implementation
  */
 
-
-/**
- * @param workField
- * @return List
- */
-
-Recruit* AddRecruit::addRecruit(vector<string> info) {   
+Recruit* AddRecruit::addRecruit(vector<string> info) {
+   
+    //recruit에 createRecruit(info) 값을 넣고
     Recruit* recruit = Recruit::createRecruit(info);
+    //RecruitCollection 에 방금 만들어진 채용 공고를 넣어준다
     RecruitCollection::getInstance()->addRecruit(recruit);
+    //회사회원이 만든 채용 공고 리스트에 추가
     ((CompanyMember*)Member::getInstance())->addToRecruitList(recruit);
+
     return recruit;
 }

ca02833e4eb8fbd3618a5cc55b43e24f55c12b10
chanheap <parc57165@naver.com>
2023-05-26 06:38:00 +0900

    AddRecruit 주석추가, CheckRegisteredRecruitUI 추가

--- Hongik-Software-Engineering/source/Boundaries/Hire/AddRecruitUI.cpp
+++ Hongik-Software-Engineering/source/Boundaries/Hire/AddRecruitUI.cpp
@@ -9,34 +9,42 @@
  * AddRecruitUI implementation
  */
 
- 
+ /*
+    함수 이름   : AddRecruitUI::startInterface()
+    기능  : Member class의 getInstance() 함수를 호출 하고 회사회원의 정보, 등록하고자 하는 채용 정보를 받아온다
+    전달 인자 : 없음
+    반환값 : 없음
+  */
 void AddRecruitUI::startInterface() {
     char companyName[MAX_STRING], workField[MAX_STRING], deadline[MAX_STRING], businessNumber [MAX_STRING];
     char numOfHire[MAX_STRING];
 
-    // 컴페니 멤버로 Member클래스의 getInstane() 호출하여 companyMember, businessNumber를 가져온다.
+
     CompanyMember* companyMember = (CompanyMember*) Member::getInstance();
-    companyMember->getInfo();
+    companyMember->getInfo(); //현재 companyMember의 companyName과 businessNumber 받아온다
 
-    //파일 입력을 받아 업무, 인원 수, 신청 마감일에 할당하고
+    
     fprintf(out_fp, "3.1. 채용 정보 등록\n>");
-    fscanf(in_fp, " %s %s %s", workField, numOfHire, deadline);
+    fscanf(in_fp, " %s %s %s", workField, numOfHire, deadline); //파일로부터 업무, 인원 수, 신청 마감일을 읽어봐서 반환
 
-    //앞에서 받은 인자들을 vector<string> info 리스트에 넣어서
-    //회사회원이 올린 채용공고로 등록을 시킨다.
+   
     vector<string> info;
     info.push_back(companyName);
     info.push_back(businessNumber);
     info.push_back(workField);
     info.push_back(numOfHire);
-    info.push_back(deadline);
+    info.push_back(deadline); // vector<string> info로 받은 값들을 리스트에 넣어서 채용 공고로 등록 시킨다.
 
-    //txt 출력 파일에 출력한다.
     fprintf(out_fp, " %s %d %s", workField, &numOfHire, deadline);
-    fprintf(out_fp, "\n");
+    fprintf(out_fp, "\n"); // 받은 결과 값을 파일에 출력.
 }
 
-
+/*
+   함수 이름   : AddRecruitUI::setControl(AddRecruit* control)
+   기능  : 외부에서 AddRecruit class (control class)의 포인터 객체를 전달 받아, AddRecruitUI(boundary class)가 AddRecruit(control class)를 참조하도록 속성값으로 저장.
+   전달 인자 : AddRecruit class의 포인터 객체
+   반환값 : 없음.
+ */
 void AddRecruitUI::setControl(AddRecruit* control) {
    
    this->control = control;
--- Hongik-Software-Engineering/source/Controls/Hire/AddRecruit.cpp
+++ Hongik-Software-Engineering/source/Controls/Hire/AddRecruit.cpp
@@ -8,18 +8,19 @@
 #include "./../../Entities/CompanyMember.h"
 
 using namespace std;
-/**
- * AddRecruit implementation
- */
-
+/*
+    함수 이름 : AddRecruit::addRecruit
+    기능   : 특정 회사회원이 등록하고자 하는 채용 정보를 받아와서 채용 정보를 등록시켜준다. 그리고 그 회사회원이 등록한 채용 정보 리스트에 추가.
+    전달 인자 : vector<string> info
+    반환값    : recruit, vector<string>타입으로 반환되며, 내용은 회사이름, 사업자번호, 업무, 인원 수, 신청 마감일이다.
+*/
 Recruit* AddRecruit::addRecruit(vector<string> info) {
    
-    //recruit에 createRecruit(info) 값을 넣고
-    Recruit* recruit = Recruit::createRecruit(info);
-    //RecruitCollection 에 방금 만들어진 채용 공고를 넣어준다
-    RecruitCollection::getInstance()->addRecruit(recruit);
-    //회사회원이 만든 채용 공고 리스트에 추가
-    ((CompanyMember*)Member::getInstance())->addToRecruitList(recruit);
+    Recruit* recruit = Recruit::createRecruit(info); //recruit에 createRecruit(info) 값을 넣고
+  
+    RecruitCollection::getInstance()->addRecruit(recruit);     //RecruitCollection 에 방금 만들어진 채용 공고를 넣어준다
+   
+    ((CompanyMember*)Member::getInstance())->addToRecruitList(recruit);  //회사회원이 만든 채용 공고 리스트에 추가
 
     return recruit;
 }
